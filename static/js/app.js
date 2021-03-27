88edbee9561ded086505cdc9f2680ab12b95f253
thierzon <thierryzonneveld@gmail.com>
2021-03-27 13:35:09 +0800

    Commit app.js

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,52 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// select table body
+var tbody = d3.select("tbody");
+
+// populate table body
+tableData.forEach((ufo) => {
+    var row = tbody.append("tr");
+    Object.values(ufo).forEach((value) => {
+      var cell = row.append("td");
+      cell.text(value);
+    });
+});
+
+// select the button
+var button = d3.select("#filter-btn");
+
+// select the form
+var form = d3.select("form");
+
+// create event handlers 
+button.on("click",runEnter);
+form.on("submit",runEnter);
+
+// event handler function for the form
+function runEnter() {
+
+    // prevent the page from refreshing
+    d3.event.preventDefault();
+    
+    // select the input element and get the raw HTML node
+    var inputElement = d3.select("#datetime");
+  
+    // get the value property of the input element
+    var inputValue = inputElement.property("value");
+  
+    console.log(inputValue);
+  
+    var filteredData = tableData.filter(date => date.datetime === inputValue);
+  
+    console.log(filteredData);
+
+    filteredData.forEach((ufo) => {
+        var row = tbody.append("tr");
+        Object.values(ufo).forEach((value) => {
+          var cell = row.append("td");
+          cell.text(value);
+        });
+    });
+
+};
