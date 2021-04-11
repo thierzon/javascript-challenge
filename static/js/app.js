2708b705731c66da0fd8e7358760d3a726a1bbc2
thierzon <thierryzonneveld@gmail.com>
2021-04-11 12:32:25 +0800

    finished level 1

--- static/js/app.js
+++ static/js/app.js
@@ -26,27 +26,38 @@ form.on("submit",runEnter);
 // event handler function for the form
 function runEnter() {
 
-    // prevent the page from refreshing
-    d3.event.preventDefault();
-    
-    // select the input element and get the raw HTML node
-    var inputElement = d3.select("#datetime");
+  // prevent the page from refreshing
+  d3.event.preventDefault();
   
-    // get the value property of the input element
-    var inputValue = inputElement.property("value");
-  
-    console.log(inputValue);
-  
+  // select the input element and get the raw HTML node
+  var inputElement = d3.select("#datetime");
+
+  // get the value property of the input element
+  var inputValue = inputElement.property("value");
+
+  console.log(inputValue);
+
+  // present data based on input value provided
+  if (inputValue === "") {
+    // no input value provided 
+    var filteredData = tableData;
+  }
+  else {
+    // filter data based on the input value provided
     var filteredData = tableData.filter(date => date.datetime === inputValue);
-  
-    console.log(filteredData);
+  };
 
-    filteredData.forEach((ufo) => {
-        var row = tbody.append("tr");
-        Object.values(ufo).forEach((value) => {
-          var cell = row.append("td");
-          cell.text(value);
-        });
+  console.log(filteredData);
+
+  // delete all data from table
+  d3.selectAll("td").remove();
+
+  // append rows for input provided
+  filteredData.forEach((ufo) => {
+    var row = tbody.append("tr");
+    Object.values(ufo).forEach((value) => {
+      var cell = row.append("td");
+      cell.text(value);
     });
-
+  });
 };
